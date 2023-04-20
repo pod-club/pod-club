bd2fb4e2c5b9930814c2c3bf66005d303183523f
aminahagi <amina.hagi@student.moringaschool.com>
2023-04-20 17:19:21 +0300

    changes made in file

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,34 +1,15 @@
 import React from "react";
-import logo from "/home/aisha/Documents/Development/phase5/pod-club/frontend/src/components/assets/PodClub__2_-removebg-preview.png"
+import logo from "/home/amina/development/projects/phase-5/pod-club/frontend/src/components/assets/PodClub__2_-removebg-preview.png"
 
 export default function Footer(){
     return(
         <div className="Footers">
             <div className="FooterBotom">
                 <div className="FooterText">
-                    <p>PodClub</p>
-                    <img src={logo} className="Lg"/>
-                </div>
-                <div className="text">
-                    <p>Home</p>
-                    {/* <a href="/home">Home</a> */}
-                    <p>Lorem</p>
-                </div>
-                <div className="text">
-                    <p>About</p>
-                    <p>Lorem Impose</p>
-                </div>
-                <div className="text">
-                    <p>Contact Us</p>
-                    <p>+254 7123456789</p>
-                </div>
-                <div className="text">
-                    <p>Service</p>
-                    <p>Team Work</p>
                 </div>
             </div>
-            <hr/>
             <div className="FooterBotom">
+            <img src={logo} className="Lg"/>
                 <div className="footerBox">
                    <i class="bi bi-youtube"></i>
                    <i class="bi bi-facebook"></i>
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -7,15 +7,13 @@ function Home() {
             <div className="contentInfo">
                 <div className="contentInfoBox">
                     <div className="contentInfoBoxText">
-                        <h1>What Hot this <br /> weekend?</h1>
+                        <h1>What's Hot this <br /> weekend?</h1>
                       
                         <p>
                             "Lorem ipsum" is a commonly used placeholder text in the design and printing<br /> industries. It is used as filler text to demonstrate the visual effects of <br />different typefaces and layouts without the distraction of meaningful content.
                         </p>
                         <div className="buttons">
-                            <button>Get Started</button>
-                            <i class="bi bi-share-fill"></i>
-                            <i class="bi bi-box-arrow-down"></i>
+                        <button className="button-get-started" style={{padding: "16px 32px", backgroundColor: "#ffffff", border: "none", borderRadius: "32px", color: "#000000", fontSize: "16px", fontWeight: "bold", cursor: "pointer", transition: "background-color 0.2s ease-in-out"}}>Get Started</button>
                         </div>
                     </div>
                 </div>
