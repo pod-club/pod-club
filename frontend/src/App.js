de4571fa1f0a264b8dd65ea973ca5d3344f6be95
aminahagi <amina.hagi@student.moringaschool.com>
2023-04-25 11:22:35 +0300

    changes made in file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,19 +4,11 @@ import Login from './components/Login';
 import Signup from './components/Signup';
 
 function App() {
-  const [showLogin, setShowLogin] = useState(true);
 
-  const handleToggle = () => {
-    setShowLogin(!showLogin);
-  };
 
   return (
     <div>
-      {showLogin ? (
-        <Login toggle={handleToggle} />
-      ) : (
-        <Signup toggle={handleToggle} />
-      )}
+    
     </div>
   );
 }
