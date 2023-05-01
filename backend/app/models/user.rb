4565b1ee5dcad05482402daebfdca781fe1d74bc
ErnestH1 <hansonernest7@gmail.com>
2023-05-02 01:18:34 +0300

    updated model associations

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
     has_secure_password
 
     has_many :chatrooms
+    has_many :chatroom_members
+    has_many :enrolled_chatrooms , through: :chatroom_members, source: :chatroom
     has_many :messages
 
 
