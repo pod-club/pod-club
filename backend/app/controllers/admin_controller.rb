2e89200ab76977939cddd76efa5dd31cf43f0d8f
e-nk <enocklagat747@gmail.com>
2023-05-02 12:04:29 +0300

    controller :admin

--- backend/app/controllers/admin_controller.rb
+++ backend/app/controllers/admin_controller.rb
@@ -1,2 +1,62 @@
 class AdminController < ApplicationController
+
+    # before_action :admin
+
+    def ban_user
+        user = User.find_by(email: params[:email])
+
+        if !user
+            render json: {message: "User not found"}, status: :not_found
+            return
+        end
+
+        if user.banned
+            render json: {message: "User already banned!"}, status: :not_found
+            return
+        end
+
+        user.banned = true
+        user.save
+
+        render json: {message: "User banned"}, status: :ok
+
+    end
+
+
+    def unban_user
+        
+        user = User.find_by(email: params[:email])
+
+        if !user
+            render json: {message: "User not found"}, status: :not_found
+            return
+        end
+
+        user.banned = false
+        user.save
+
+        render json: {message: "User unbanned"}, status: :ok
+
+    end
+
+    def banned_users
+        users = User.where(banned: true)
+
+        render json: users
+    end
+
+    def reports
+        reports = Report.all
+        render json: reports, status: :ok
+    end
+
+    def view_chatrooms
+        chatrooms = Chatrooms.all
+        render json: chatrooms
+    end
+    private
+
+    def admin?
+        current_user.admin?
+    end
 end
